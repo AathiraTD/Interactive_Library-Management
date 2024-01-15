2074cc80ea78e2f8402d0f405443b2b8f8edaa3a
angharadS <102169539+angharadS@users.noreply.github.com>
2024-01-15 03:20:18 +0000

    Delete JavaDoc directory

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"bcu.cmp5332.librarysystem.commands"},{"l":"bcu.cmp5332.librarysystem.data"},{"l":"bcu.cmp5332.librarysystem.gui"},{"l":"bcu.cmp5332.librarysystem.main"},{"l":"bcu.cmp5332.librarysystem.model"},{"l":"bcu.cmp5332.librarysystem.test"}];updateSearchResults();
